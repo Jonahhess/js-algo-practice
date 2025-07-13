98aeb25df624cf47fe40cf44a0c9d9c0c5b9b93d
jonahhess <jonahhessdev@gmail.com>
2025-07-13 18:12:14 +0300

    solved

--- Easy/calculator.js
+++ Easy/calculator.js
@@ -11,8 +11,26 @@ Notes
 If the input tries to divide by 0, return: "Can't divide by 0!"
 */
 
-function calculator( /*args*/ ) {
-  //your code
+function calculator(num1, op, num2) {
+  const add = (a, b) => a + b;
+  const sub = (a, b) => a - b;
+  const mult = (a, b) => a * b;
+  const div = (a, b) => {
+    if (b === 0) {
+      return "Can't divide by 0!";
+    }
+    return a / b;
+  };
+  const ops = new Map([
+    ["+", add],
+    ["-", sub],
+    ["*", mult],
+    ["/", div],
+  ]);
+
+  if (ops.has(op)) {
+    return ops.get(op)(num1, num2);
+  }
 }
 
 exports.solution = calculator;
