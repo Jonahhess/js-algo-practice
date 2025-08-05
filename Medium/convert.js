bf96bb93af003b3e52ccebeda22c3bb930ab858c
jonahhess <jonahhessdev@gmail.com>
2025-08-05 14:00:06 +0300

    solved

--- Medium/convert.js
+++ Medium/convert.js
@@ -12,8 +12,34 @@ Round to the nearest integer.
 If the input is incorrect, return "Error".
 */
 
-function convert( /*args*/ ) {
-  //your code
+// const TEMPERATURE_RANGE = {
+//   "°F": { min: -459.67, max: 212 },
+//   "°C": { min: -273.15, max: 100 },
+// };
+
+// function temperatureInRange(numberComponent, unitComponent) {
+//   if (!(unitComponent in TEMPERATURE_RANGE)) throw new Error("unit not valid");
+
+//   return (
+//     numberComponent >= TEMPERATURE_RANGE[unitComponent].min &&
+//     numberComponent <= TEMPERATURE_RANGE[unitComponent].max
+//   );
+// }
+
+function convert(temp) {
+  if (typeof temp !== "string") return "Error";
+
+  const numberComponent = Number.parseInt(temp.slice(0, -2));
+  if (Number.isNaN(numberComponent)) return "Error";
+
+  const unitComponent = temp.slice(-2);
+  if (!["°F", "°C"].includes(unitComponent)) return "Error";
+
+  // if (!temperatureInRange(numberComponent, unitComponent)) return "Error"
+
+  return unitComponent === "°F"
+    ? Math.round((numberComponent - 32) / 1.8).toString() + "°C"
+    : Math.round(numberComponent * 1.8 + 32).toString() + "°F";
 }
 
 exports.solution = convert;
